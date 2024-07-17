8c5a49ad94a6c7482f43c3845a254183cf9d1c60
hayaty88 <payagram01@gmail.com>
2024-07-18 03:10:26 +1000

    Add Navbar styles and integrate FontAwesome icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faFilePen,
+  faRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/postArticle">Post Article</Link>
-      <Link to="/login">Login</Link>
+      <Link to="/">
+        <FontAwesomeIcon icon={faHouse} />
+        &nbsp;Home
+      </Link>
+      <Link to="/postArticle">
+        <FontAwesomeIcon icon={faFilePen} />
+        &nbsp;Post Article
+      </Link>
+      <Link to="/login">
+        <FontAwesomeIcon icon={faRightToBracket} />
+        &nbsp;Login
+      </Link>
     </nav>
   );
 };
