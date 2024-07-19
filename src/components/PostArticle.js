07cde6abf9634308cdedef2e0ed70935a380646a
hayaty88 <payagram01@gmail.com>
2024-07-20 09:25:22 +1000

    Catch input data

--- src/components/PostArticle.js
+++ src/components/PostArticle.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./PostArticle.css";
 
 const PostArticle = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const sendPostData = () => {
+    console.log(title);
+    console.log(content);
+  };
+
+  useEffect(() => {
+    const postTitleEl = document.getElementById("postTitle");
+    const postContentEl = document.getElementById("postContent");
+
+    const handleTitleChange = (e) => {
+      console.log("postTitleEl: " + e.target.value);
+    };
+
+    const handleContentChange = (e) => {
+      console.log("postContentEl: " + e.target.value);
+    };
+
+    postTitleEl.addEventListener("change", handleTitleChange);
+
+    postContentEl.addEventListener("change", handleContentChange);
+
+    return () => {
+      postTitleEl.removeEventListener("change", handleTitleChange);
+
+      postContentEl.removeEventListener("change", handleContentChange);
+    };
+  }, []);
+
   return (
     <div className="postBlock">
       <div className="postContainer">
@@ -9,21 +39,27 @@ const PostArticle = () => {
         <div className="postPart">
           <h2 className="postHeading">Title</h2>
           <input
+            id="postTitle"
             className="postTitle"
             type="text"
             placeholder="Enter an title"
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="postPart">
           <p className="postHeading">Content</p>
           <textarea
+            id="postContent"
             className="postContent"
             cols="30"
             rows="10"
             placeholder="Enter texts"
+            onChange={(e) => setContent(e.target.value)}
           ></textarea>
         </div>
-        <button className="postButton">Post</button>
+        <button className="postButton" onClick={sendPostData}>
+          Post
+        </button>
       </div>
     </div>
   );
