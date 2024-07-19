4fc5b563c818839a49a01a5c02a64defab08f59e
hayaty88 <payagram01@gmail.com>
2024-07-19 23:59:14 +1000

    Implement Google Authentication and preserve user session state

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,25 @@
+import { signInWithPopup } from "firebase/auth";
 import React from "react";
+import { auth, provider } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const loginWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
+  return (
+    <div>
+      <p>Start with Login.</p>
+      <button type="button" onClick={loginWithGoogle}>
+        Login with Google
+      </button>
+    </div>
+  );
 };
 
 export default Login;
