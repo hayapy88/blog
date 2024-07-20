a36435301c4497f3d63c5d9b5a94198a2aad309f
hayaty88 <payagram01@gmail.com>
2024-07-20 18:11:38 +1000

    Add styles for Home page and adjust for PostArticle too

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,35 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  const deletePostData = () => {
+    console.log("Clicked Delete button");
+  };
+  return (
+    <div className="postBlock">
+      <div className="postContainer postContainer--home">
+        <div className="postPart">
+          <h2 className="postHeading postHeading--title">
+            This is a Title This is a Title This is a Title
+          </h2>
+        </div>
+        <div className="postPart">
+          <p className="postHeading">
+            This is a content texts. This is a content texts. This is a content
+            texts. This is a content texts. This is a content texts.
+          </p>
+        </div>
+        <div className="postPart">
+          <p className="postHeading postHeading--author">@Author name</p>
+        </div>
+        <div className="postPart">
+          <button className="deleteButton" onClick={deletePostData}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
