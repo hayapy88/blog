8071ce0ca7f0d4dda987a9f3579cb1e823d10efc
hayaty88 <payagram01@gmail.com>
2024-07-25 06:01:56 +1000

    Integrate fetching data from Firestore and outputing them and adjust styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      // console.log(data);
+      // console.log(data.docs);
+      // console.log(data.docs.map((doc) => ({ doc })));
+      // console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
   const deletePostData = () => {
     console.log("Clicked Delete button");
   };
   return (
     <div className="homePage postBlock">
-      <div className="postContainer postContainer--home">
-        <div className="postPart">
-          <h2 className="postHeading postHeading--title">
-            This is a Title This is a Title This is a Title
-          </h2>
-        </div>
-        <div className="postPart">
-          <p className="postHeading">
-            This is a content texts. This is a content texts. This is a content
-            texts. This is a content texts. This is a content texts. This is a
-            content texts. This is a content texts. This is a content texts.
-            This is a content texts. This is a content texts. This is a content
-            texts.
-          </p>
-        </div>
-        <div className="postPart postPart--authorAndDelete">
-          <p className="postHeading postHeading--author">@Author name</p>
-          <button className="deleteButton" onClick={deletePostData}>
-            Delete
-          </button>
-        </div>
-      </div>
+      {postList.map((post) => {
+        return (
+          <div className="postContainer postContainer--home" key={post.id}>
+            <div className="postPart">
+              <h2 className="postTitle postHeading postHeading--title">
+                {post.title}
+              </h2>
+            </div>
+            <div className="postPart">
+              <p className="postContent postHeading">{post.content}</p>
+            </div>
+            <div className="postPart postPart--authorAndDelete">
+              <p className="postAuthor postHeading postHeading--author">
+                @{post.author.username}
+              </p>
+              <button className="deleteButton" onClick={deletePostData}>
+                Delete
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
