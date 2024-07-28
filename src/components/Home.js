e26d181f8a870cde351fea770b5862b98a8d9746
hayaty88 <payagram01@gmail.com>
2024-07-28 23:35:52 +1000

    Process date format with date-fns

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,8 +10,18 @@ import {
   serverTimestamp,
   updateDoc,
 } from "firebase/firestore";
+import { format } from "date-fns";
 import "./Common.css";
 import "./Home.css";
+
+const formatTimestamp = (timestamp) => {
+  const date = timestamp.toDate();
+  return format(date, "dd MMMM yyyy - HH:mm");
+};
+
+const PostDate = ({ timestamp }) => {
+  return <p>{formatTimestamp(timestamp)}</p>;
+};
 
 const Home = () => {
   const [postList, setPostList] = useState([]);
@@ -39,6 +49,7 @@ const Home = () => {
         ...doc.data(),
       }));
 
+      console.log("docs");
       console.log(docs);
 
       const updates = docs.map(async (document) => {
@@ -72,12 +83,7 @@ const Home = () => {
             <div className="postPart">
               <p className="postContent postHeading">{post.content}</p>
             </div>
-            {/* <p>{currentUser.uid}</p> */}
-            <p>
-              {post.createTime
-                ? post.createTime.toDate().toString()
-                : "Updating..."}
-            </p>
+            <PostDate timestamp={post.createTime} />
             <div className="postPart postPart--authorAndDelete">
               <p className="postAuthor postHeading postHeading--author">
                 @{post.author.username}
